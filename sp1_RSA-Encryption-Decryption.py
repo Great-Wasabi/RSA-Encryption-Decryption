6fe842b7dca3b6d26828ca4a4afa023d036dbe25
Pipkck <119881640+Great-Wasabi@users.noreply.github.com>
2023-05-09 20:56:53 -0700

    Update sp1_RSA-Encryption-Decryption.py

--- sp1_RSA-Encryption-Decryption.py
+++ sp1_RSA-Encryption-Decryption.py
@@ -16,33 +16,62 @@ def generatePrime(min, max):
         prime = random.randint(min, max)
     return prime
 
-def mod_inverse(e, phi):
+def generate_p_q_primes(min, max):
+    p, q = generatePrime(min, max), generatePrime(min, max)
+    while p == q:
+        q = generatePrime(min, max)
+    return p, q
+
+def generate_n(p, q):
+    return p * q
+
+def generate_phi_n(p, q):
+    return (p-1) * (q-1)
+
+def generatePublicKey(phi_n):
+    e = random.randint(3, phi_n-1)
+    while math.gcd(e, phi_n) != 1:
+        e = random.randint(3, phi_n-1)
+    return e    
+
+def generatePrivateKey(e, phi):
     for d in range(3, phi):
         if (d * e) % phi == 1:
             return d
     raise ValueError("modular inverse does not exist")
 
-p, q = generatePrime(1000, 10000), generatePrime(1000, 10000)
-while p == q:
-    q = generatePrime(1000, 10000)
+def encrypt(message, e, n):
+    return [pow(ord(char), e, n) for char in message]
 
-n = p * q 
-phi_n = (p-1) * (q-1)
+def decrypt(cipher, d, n):
+    return "".join([chr(pow(char, d, n)) for char in cipherText])
 
-e = random.randint(3, phi_n-1)
-while math.gcd(e, phi_n) != 1:
-    e = random.randint(3, phi_n-1)
+if __name__ == '__main__':
+    p, q = generate_p_q_primes(10000, 100000)
+    n = generate_n(p, q)
+    phi_n = generate_phi_n(p, q)
+    e = generatePublicKey(phi_n)
+    d = generatePrivateKey(e, phi_n)
 
-d = mod_inverse(e, phi_n)
+#    print("p (Generated Number between 10000 and 100000): ", p)
+#    print("q (Generated Number between 10000 and 100000): ", q)
+#    print("n (p * q): ", n)
+#    print("phi_n (p-1 * q-1): ", phi_n)
+#    print("Public Key (e) (e is a number between 3 and phi_n-1 that is not coprime to phi_n): ", e)
+#    print("Private Key (d) (d is a number such that (e*d)%phi_n = 1): ", d)
+#    print("Check : ", e*d%phi_n)
+    
 
-print("Public Key (e) : ", e)
-print("Private Key (d) : ", d)
-print("n : ", n)
-print("phi_n : ", phi_n)
-print("p : ", p)
-print("q : ", q)
+    message = "Hello World"
+    print("Initial Message: ",message)
+    cipherText =  encrypt(message, e, n)
+    print("Cipher Text: ", cipherText)
+    message_final = decrypt(cipherText, d, n)
+    print(message_final)
 
-message = "Hello World"
-messageEncoded = [ord(char) for char in message]
-cipherText = [pow(char, e, n) for char in messageEncoded]
-print(cipherText)
+
+
+
+
+
+
