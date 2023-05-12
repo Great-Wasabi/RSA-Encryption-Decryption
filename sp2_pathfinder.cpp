910be43350b9baa43989678a35f6e27f041628ba
Pipkck <119881640+Great-Wasabi@users.noreply.github.com>
2023-05-11 17:37:52 -0700

    Update sp2_pathfinder.cpp
    Modified pathfinder to have faster, more modular and coherent language.

--- sp2_pathfinder.cpp
+++ sp2_pathfinder.cpp
@@ -1,11 +1,3 @@
-//Assignment 2
-//Name : Ujjwal Baranwal
-//Class : T/Th 4:30
-//Professor : Selgrad
-//Usage: ./[executable filename]
-//Description: This code finds a path from the start position to the goal position
-//Date: 27 October 2021
-
 #include <iomanip>
 #include <iostream>
 #include <stdlib.h>
@@ -16,7 +8,6 @@
 
 using namespace std;
 
-// struct Node for storing coords
 struct Node
 {
    public:
@@ -55,140 +46,102 @@ struct Queue
     }
 };
 
-//greed path finding function based on recursion
-void greed(int** arr, int W, int H,int X,int Y,char** charr)
+int** reformMatrix(int** matrix, int height, int width, int impass, int value) 
 {
-    int newX, newY;
-    int mini = arr[Y][X];
-    if(mini == 1)
-        return;
-    else if(arr[Y-1][X-1] < mini && arr[Y-1][X-1] > 1)
-    {
-        charr[Y-1][X-1] = '*';
-        newX = X-1;newY = Y-1;
-        greed(arr, W, H, newX, newY, charr);
-    }
-    else if(arr[Y][X-1] < mini && arr[Y][X-1] > 1)
-    {
-        charr[Y][X-1] = '*';
-        newX = X-1;newY = Y;
-        greed(arr, W, H, newX, newY, charr);
-    }
-    else if(arr[Y+1][X-1] < mini && arr[Y+1][X-1] > 1)
-    {
-        charr[Y+1][X-1] = '*';
-        newX = X-1;newY = Y+1;
-        greed(arr, W, H, newX, newY, charr);
-    }
-    else if(arr[Y-1][X] < mini && arr[Y-1][X] > 1)
-    {
-        charr[Y-1][X] = '*';
-        newX = X;newY = Y-1;
-        greed(arr, W, H, newX, newY, charr);
-    }
-    else if(arr[Y][X] < mini && arr[Y][X] > 1)
-    {
-        charr[Y][X] = '*';
-        newX = X;newY = Y;
-        greed(arr, W, H, newX, newY, charr);
-    }
-    else if(arr[Y+1][X] < mini && arr[Y+1][X] > 1)
-    {
-        charr[Y+1][X] = '*';
-        newX = X;newY = Y+1;
-        greed(arr, W, H, newX, newY, charr);
-    }
-    else if(arr[Y-1][X+1] < mini && arr[Y-1][X+1] > 1)
-    {
-        charr[Y-1][X+1] = '*';
-        newX = X+1;newY = Y-1;
-        greed(arr, W, H, newX, newY, charr);
-    }
-    else if(arr[Y][X+1] < mini && arr[Y][X+1] > 1)
-    {
-        charr[Y][X+1] = '*';
-        newX = X+1;newY = Y;
-        greed(arr, W, H, newX, newY, charr);
-    }
-    else if(arr[Y+1][X+1] < mini && arr[Y+1][X+1] > 1)
-    {
-        charr[Y+1][X+1] = '*';
-        newX = X+1;newY = Y+1;
-        greed(arr, W, H, newX, newY, charr);
-    }
+    srand (time(NULL));
 
+    matrix = new int*[height];
+    for (int i = 0; i < height; i++) 
+        matrix[i] = new int[width];
 
-}
-
-//  function to print int array
-void PrintEnvironmen(int** arr, int W, int H) {
-    cout << "arr:\n";
-    for (int i = 0; i < H; i++) {
-        for (int j = 0; j < W; j++) {
-            cout << setw(2) << arr[i][j] << " ";
+    if(impass == 100)
+    {
+        for (int i = 0; i < height; i++) 
+            for (int j = 0; j < width; j++) 
+                matrix[i][j] = value;
+    }
+    else
+    {
+        int log = (impass * height * width)/100;
+        for (int i = 0; i < log;) 
+        {
+            int randHeight = rand() % height, randWidth = rand() % width;
+            if (matrix[randHeight][randWidth] != value) 
+            {
+                matrix[randHeight][randWidth] = value;
+                i++;
+            }
         }
-        cout << "\n";
     }
+    return matrix;
 }
-// function to print char array
-void CPrintEnvironmen(char** arr, int W, int H) {
-    cout << "arr:\n";
-    for (int i = 0; i < H; i++) {
-        for (int j = 0; j < W; j++) {
-            cout << setw(2) << arr[i][j] << " ";
+
+
+void printMatrix(int** matrix, int height, int width) 
+{
+    for (int i = 0; i < height; i++) 
+    {
+        for (int j = 0; j < width; j++) 
+        {
+            if(matrix[i][j]==-1 || matrix[i][j]==10)
+                cout << matrix[i][j] << setw(4) << "  ";
+            else
+                cout << " " <<matrix[i][j] << setw(4) << " ";
         }
-        cout << "\n";
+            
+        cout << endl;
     }
 }
 
-// find and increment moore neighbour
-void Moore(int** arr, int W, int H, int X, int Y) {
+void Moore(int** matrix, int W, int H, int goalX, int goalY) 
+{
     Queue q(W * H);
-    q.push(Y, X);
+    q.push(goalY, goalX);
 
-    while (!q.empty()) {
+    while (!q.empty()) 
+    {
         Node curr = q.pop();
         int i = curr.x, j = curr.y;
         bool north = (i > 0), south = (i < H - 1), east = (j < W - 1), west = (j > 0);
 
-        if (north && east && !arr[i - 1][j + 1])
+        if (north && east && !matrix[i - 1][j + 1])
         {
-            arr[i - 1][j + 1] = arr[i][j] + 1;
+            matrix[i - 1][j + 1] = matrix[i][j] + 1;
             q.push(i - 1, j + 1);
         }
-        if (north && west && !arr[i - 1][j - 1])
+        if (north && west && !matrix[i - 1][j - 1])
         {
-            arr[i - 1][j - 1] = arr[i][j] + 1;
+            matrix[i - 1][j - 1] = matrix[i][j] + 1;
             q.push(i - 1, j - 1);
         }
-        if (south && east && !arr[i + 1][j + 1])
+        if (south && east && !matrix[i + 1][j + 1])
         {
-            arr[i + 1][j + 1] = arr[i][j] + 1;
+            matrix[i + 1][j + 1] = matrix[i][j] + 1;
             q.push(i + 1, j + 1);
         }
-        if (south && west && !arr[i + 1][j - 1])
+        if (south && west && !matrix[i + 1][j - 1])
         {
-            arr[i + 1][j - 1] = arr[i][j] + 1;
+            matrix[i + 1][j - 1] = matrix[i][j] + 1;
             q.push(i + 1, j - 1);
         }
-        if (north && !arr[i - 1][j])
+        if (north && !matrix[i - 1][j])
         {
-            arr[i - 1][j] = arr[i][j] + 1;
+            matrix[i - 1][j] = matrix[i][j] + 1;
             q.push(i - 1, j);
         }
-        if (south && !arr[i + 1][j])
+        if (south && !matrix[i + 1][j])
         {
-            arr[i + 1][j] = arr[i][j] + 1;
+            matrix[i + 1][j] = matrix[i][j] + 1;
             q.push(i + 1, j);
         }
-        if (east && !arr[i][j + 1])
+        if (east && !matrix[i][j + 1])
         {
-            arr[i][j + 1] = arr[i][j] + 1;
+            matrix[i][j + 1] = matrix[i][j] + 1;
             q.push(i, j + 1);
         }
-        if (west && !arr[i][j - 1])
+        if (west && !matrix[i][j - 1])
         {
-            arr[i][j - 1] = arr[i][j] + 1;
+            matrix[i][j - 1] = matrix[i][j] + 1;
             q.push(i, j - 1);
         }
     }
@@ -196,30 +149,19 @@ void Moore(int** arr, int W, int H, int X, int Y) {
 
 int main()
 {
-    srand (time(NULL));
-
-    // vars
-    int H, W, X, Y, impass, log, check=1;
+    int height, width;
+    int impass;
+    int goalX, goalY, startX, startY;
     char choice;
-    //input width and height
-    cout << "Welcome to CSE240 WaveFront Pather" <<endl <<endl;
+    
+    cout << "Welcome to the WaveFront Pather" <<endl <<endl;
     cout << "Please tell me about the grid you want to generate." << endl;
-    cout << "Enter the width (at least 10): ";
-    cin >> W;
     cout << "Enter the height (at least 10): ";
-    cin >> H;
-    // dynamically initialize array
-    int** arr = (int**)malloc(H * sizeof(int*));
-    for (int i = 0; i < H; i++)
-        arr[i] = (int*)malloc(W * sizeof(int));
+    cin >> height;
+    cout << "Enter the width (at least 10): ";
+    cin >> width;
 
-    for (int i = 0; i < H; i++)
-    {
-        for (int j = 0; j < W; j++)
-        {
-            arr[i][j] = 0;
-        }
-    }
+    int** graphArray = reformMatrix(graphArray, height, width, 100, 0);
 
     while(true)
     {
@@ -235,78 +177,46 @@ int main()
         else break;
     }
 
+    graphArray = reformMatrix(graphArray, height, width, impass, -1);
 
-    log = (impass * H * W)/100;
+    //graphArray[height-3-1][4] = 99;
+    //printMatrix(graphArray, height, width);
 
-
-    int i = 0;
-    while(i<log)
-    {
-        if(arr[rand()%H][rand()%W] != -1)
-        {
-            arr[rand()%H][rand()%W] = -1;
-            i++;
-        }
-    }
-
-    while(check!=0)
+    while(true)
     {
         cout << "Please enter the Goal Position X: ";
-        cin >> X;
+        cin >> goalX;
         cout << "Please enter the Goal Position Y: ";
-        cin >> Y;
+        cin >> goalY;
 
-        if(arr[Y][X]!=-1)
-        {
-            arr[Y][X] = 1;
-            check = 0;
-        }
+        if(graphArray[height-goalY-1][goalX]==-1 || goalY >= height || goalX >= width)
+            cout << "Sorry, that position is inside an obstacle or Out of Bounds" << endl;
         else
-            cout << "Sorry, that position is inside an obstacle" << endl;;
+        {
+            int rgoalY = height-goalY-1;
+            int rgoalX = goalX;
+            break;
+        }     
     }
 
-    Moore(arr, W, H, X, Y);
-
-    PrintEnvironmen(arr, W, H);
-
-    char** parr = (char**)malloc(H * sizeof(char*));
-    for (int i = 0; i < H; i++)
-        parr[i] = (char*)malloc(W * sizeof(char));
-
-    int sX, sY;
-    check = 1;
-    while(check!=0)
+    while(true)
     {
         cout << "Please enter the Start Position X: ";
-        cin >> sX;
+        cin >> startX;
         cout << "Please enter the Start Position Y: ";
-        cin >> sY;
+        cin >> startY;
 
-        if(arr[sY][sX]!=-1)
-        {
-            parr[sY][sX] = '@';
-            check = 0;
-        }
+        if(graphArray[height-startY-1][startX]==-1 || startY >= height || startX >= width)
+            cout << "Sorry, that position is inside an obstacle or Out of Bounds" << endl;
         else
-            cout << "Sorry, that position is inside an obstacle" << endl;;
-    }
-
-    for (int i = 0; i < H; i++)
-    {
-        for (int j = 0; j < W; j++)
         {
-            if(arr[i][j] == -1)
-                parr[i][j] = '#';
-            else if(arr[i][j] == 1)
-                parr[i][j] = '$';
-            else
-                parr[i][j] = ' ';
-        }
+            int rstartY = height-startY-1;
+            int rstartX = startX;
+            break;
+        }     
     }
-    parr[sY][sX] = '@';
 
-    greed(arr, W, H, sX, sY, parr);
-    CPrintEnvironmen(parr, W, H);
+
 
     return 0;
 }
